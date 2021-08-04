132c525a47805bdc2924b7ee7a3349b883205cdc
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-04 12:41:59 -0400

    Metodos info rest fire base

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,33 @@ export class InfoPaginaService {
 
   info : InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) { 
     console.log('Servicio de infoPagina listo');
 
     //leer el archivo
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  //metodos carga desde firebase
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) =>{
         this.cargada = true;
         this.info=resp;
-        console.log(resp)
-      })
+          
+        });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-portfolio-81ea5-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) =>{
+        
+        this.equipo = resp;
+          console.log(resp)
+        });
   }
 }
+
