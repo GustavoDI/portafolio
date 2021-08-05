bc5b5908ea8e76b8650785f8f9ce9f37b0ef5b7e
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-04 21:38:28 -0400

    agregar import producto

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +16,11 @@ export class ProductosService {
 
   private cargarProductos(){
     this.http.get('https://angular-portfolio-81ea5-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe((resp:any) =>{
-      console.log(resp);
-      this.cargado = false;
-    })
+      .subscribe( (resp: any) => {
+        console.log(resp);
+        this.cargado = false;
+    });
 
-  }
+  };
 
-}
+};
