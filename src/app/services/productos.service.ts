3d6c70f753487963bb639fe5065ec98506c1daed
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-06 18:36:53 -0400

    Filtrar productos

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,12 +10,14 @@ export class ProductosService {
 
   cargado = true;
   productos : Producto[] = [];
+  productosFiltrado: Producto[] = []; //Recordar que esto viene desde la interface
   
   constructor( private http: HttpClient) {
     this.cargarProductos();
    }
 
   private cargarProductos(){
+    
     this.http.get('https://angular-portfolio-81ea5-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe( (resp: any) => {
         console.log(resp);
@@ -26,6 +28,35 @@ export class ProductosService {
   };
   getProducto(id: string){
     return this.http.get(`https://angular-portfolio-81ea5-default-rtdb.firebaseio.com/productos/${id}.json`);
+  };
+
+  buscarProducto(termino: string){
+
+    if (this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos();
+      this.filtrarProductos(termino);
+    }else{
+      //aplicar filtro
+      this.filtrarProductos(termino);
+    };
+    this.productosFiltrado = this.productos.filter(producto =>{
+      return true;
+    });
+    console.log(this.productosFiltrado);
+
+  };
+  private filtrarProductos(termino: string){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino )>=0 || tituloLower.indexOf(termino)>=0) {
+        this.productosFiltrado.push(prod);
+      }
+    })
   };
 
 };
