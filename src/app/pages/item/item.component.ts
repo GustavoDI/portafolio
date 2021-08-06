712c1f79c44ea00ae99e2129de88fae51fe423c5
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-05 21:29:03 -0400

    construccion de routas y productos de servicios

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoCompleto } from '../../interfaces/producto-descrip.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute,
+               private productoService: ProductosService) { }
 
   ngOnInit(): void {
-  }
+    // aqui normalmente se llama la instruccion
+    this.route.params
+      .subscribe(parametros =>{
+        console.log(parametros ['id']);
+        this.productoService.getProducto(parametros['id'])
+          .subscribe( (producto : any) => {
+            console.log(producto);
+          });
+
+      });
+  };
 
 }
